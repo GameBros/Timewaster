aad29df01f19a2f49e2caf87443a728f252ae7bd
steven.grohe95@googlemail.com <steven.grohe95@googlemail.com>
2015-05-21 00:34:09 +0200

    microgame1 update

--- Assets/game1GuyControl.cs
+++ Assets/game1GuyControl.cs
@@ -1,18 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class game1BlueGuyControl : MonoBehaviour {
+public class game1GuyControl : MonoBehaviour {
 	public float moveSp;
 	public float rotateSp;
+	public bool goodGuy;
+	public GameObject ashParticles;
+	private Vector3 dir;
 
 	// Use this for initialization
 	void Start () {
-
+		dir = Vector3.right;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		transform.Translate(Vector3.right*moveSp, Space.World);
+		transform.Translate(dir*moveSp, Space.World);
 		transform.Rotate(new Vector3(0,0,1)*rotateSp);
 	}
 }
